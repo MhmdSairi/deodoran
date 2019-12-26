c8ccb73d5211b3b57581a10ae7970c91739cdbcd
MhmdSairi <40028734+MhmdSairi@users.noreply.github.com>
2019-12-25 18:20:27 -0800

    Update index.php

--- index.php
+++ index.php
@@ -20,9 +20,9 @@ session_start(); #list: key, msisdn, otp, secret_token
 }
 
 .button {
-  background-color: transparent; 
-  color: green; 
-  border: 2px solid #4CAF50;
+  background-color: transparent;
+  color: green;
+  border: 2px solid #020101;
   border-radius: 12px;
 }
 a {
@@ -55,24 +55,24 @@ a {
 </head>
 <?php
     date_default_timezone_set('Asia/Jakarta');
-    
+
     require_once('config.php');
     require('class.php');
-    
+
     $err    = NULL;
     $ress   = NULL;
-    
+
     if (isset($_POST) and isset($_POST['do'])){
-        
+
         switch($_POST['do']){
-            
+
             default: die(); exit(); break;
 
             case "CHANGE":{
                 $key    = $_SESSION['key'];
                 $msisdn = $_SESSION['msisdn'];
                 $tipe   = $_SESSION['tipe'];
-                
+
                 unset($_SESSION['key']);
                 unset($_SESSION['tipe']);
                 unset($_SESSION['msisdn']);
@@ -81,19 +81,19 @@ a {
                 session_destroy();
             }
             break;
-            
+
             case "LOGOUT":{
-                
+
                 $key            = $_SESSION['key'];
                 $msisdn         = $_SESSION['msisdn'];
                 $tipe           = $_SESSION['tipe'];
                 $otp            = $_SESSION['otp'];
                 $secret_token   = $_SESSION['secret_token'];
-                
-                
+
+
                 $tsel = new MyTsel();
                 $tsel->logout($secret_token, $tipe);
-                
+
                 unset($_SESSION['key']);
                 unset($_SESSION['tipe']);
                 unset($_SESSION['msisdn']);
@@ -102,20 +102,20 @@ a {
                 session_destroy();
             }
             break;
-            
-            
+
+
             case "GETOTP":{
                 $key    = $_POST['key'];
                 $msisdn = $_POST['msisdn'];
-                
-                
+
+
                 if ($key != privatekey){die("Error: wrong key");}
                 $tsel = new MyTsel();
                 if ($tsel->get_otp($msisdn) == "SUKSES"){
-                    
+
                     session_regenerate_id();
                     $_SESSION['key'] = $key;
-                    $_SESSION['msisdn'] = $msisdn;                    
+                    $_SESSION['msisdn'] = $msisdn;
                     session_write_close();
 
                 }
@@ -125,35 +125,35 @@ a {
                 }
             }
             break;
-            
+
             case "LOGIN":{
                 $key    = $_SESSION['key'];
                 $msisdn = $_SESSION['msisdn'];
                 $tipe   = $_POST['tipe'];
                 $otp    = $_POST['otp'];
-                
+
                 //if ($key != privatekey){die("Error: wrong key");}
                 $tsel = new MyTsel();
                 $login = $tsel->login($msisdn, $otp, $tipe);
-                
-                
+
+
                 if (strlen($login) > 0){
 
                     $secret_token               = trim(preg_replace('/\s+/', ' ', $login));
                     $_SESSION['otp']            = $otp;
                     $_SESSION['secret_token']   = $secret_token;
                     $_SESSION['tipe']           = $tipe;
-                    
-                    
+
+
                 } else {
                     //echo $login;
                     $err = $login;
                 }
 
-                
+
             }
             break;
-            
+
             case "BUY_PKG":{
                 $key            = $_SESSION['key'];
                 $msisdn         = $_SESSION['msisdn'];
@@ -161,7 +161,7 @@ a {
                 $secret_token   = $_SESSION['secret_token'];
                 $pkgid          = $_POST['pkgid'];
                 $transactionid  = $_POST['transactionid'];
-                
+
                 switch($_POST['pkgid']){
                 case '1':
                     $pkgidman = $_POST['pkgidman'];
@@ -172,12 +172,12 @@ a {
                     $tsel = new MyTsel();
                     $ress = "PKGID: <b>".$pkgid."</b><br>Result: ".$tsel->buy_pkg($secret_token, $pkgid, $transactionid, $tipe);
                 }
-                
+
             }
             break;
-            
+
         }
-        
+
     }
 ?>
 
@@ -208,7 +208,7 @@ GET OTP
 </span></button>
 </div>
 <!-- <input type="submit" name="do" value="GETOTP"></input> -->
-<?php if(!empty($err)) echo $err ?> 
+<?php if(!empty($err)) echo $err ?>
 <!--     </pre> -->
 </form>
 </div>
@@ -222,7 +222,7 @@ GET OTP
 <div class="wrap-contact100">
 <form class="contact100-form validate-form" method="POST">
 <span class="contact100-form-title">
-Dor Tsel TPP
+DOR TSEL V2
 </span>
     <center>
 <label class="radio-container m-r-45">VMP
@@ -274,13 +274,8 @@ OTP:&nbsp;<?= $_SESSION['otp']."<br>" ?>
 <hr>
 
 PILIH&nbsp;PAKET:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<select name="pkgid" onchange="if (this.value=='1'){this.form['pkgidman'].style.visibility='visible'}else {this.form['pkgidman'].style.visibility='hidden'};" style="width: 50%;">
-  <option value="00009382">OMG! 1GB 2hari Rp 10</option>
-  <option value="00007333">OMG! 30gb 30k</option>
-  <option value="00016036">OMG! 5gb 10k</option>
-  <option value="00016030">OMG! 10gb 10k</option>
-  <option value="00016199">AddMax 30gb 30k 30hr</option>
-  <option value="00015185">Gigamax 6gb 25k 30hr</option>
-  <option value="00016038">Maxtrem 5gb 10k</option>
+  <option value="00016038">OMG V1! 5GB 30hari 10K</option>
+  <option value="00016036">OMG V2! 5GB 30hari 10K</option>
   <option value="1">Manual ID</option>
 </select><br>
 PKGID:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" placeholder="Manual Id" name="pkgidman"  style="width: 50%; visibility:visible;"></input><br>
